41ed4b4026f5b90f0cc600d9c806c4c763ac941f
vito <vito.ombero@gmail.com>
2020-11-10 22:09:13 +0300

    remove garbage

--- 05-glrender-service/include/engine/service/game_meta_info/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_01_GUESS_NUMBER_PROTO_VERSION_H
-#define INC_01_GUESS_NUMBER_PROTO_VERSION_H
-#include <cstdint>
-struct Version{
-    uint8_t major;
-    uint8_t minor;
-    uint8_t patch;
-};
-#endif //INC_01_GUESS_NUMBER_PROTO_VERSION_H
